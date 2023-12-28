c65e0de3b107ba828cfba61d0971f9921d532e44
BitCrusader <elikramergm@gmail.com>
2023-12-28 15:12:47 -0800

    Add QChar typemap

---
+++ src/QtGen/TypeMaps/QChar.cs
@@ -0,0 +1,24 @@
+﻿using CppSharp.AST;
+using CppSharp.Generators.CSharp;
+using CppSharp.Types;
+using Type = CppSharp.AST.Type;
+
+namespace QtGen.TypeMaps;
+
+[TypeMap("QChar")]
+class QChar : TypeMap
+{
+    public override Type CSharpSignatureType(TypePrinterContext ctx) => new CustomType("char");
+
+    public override void CSharpMarshalToManaged(CSharpMarshalContext ctx)
+    {
+        ctx.Return.Write($"*(char*)(&{ctx.ReturnVarName})");
+    }
+
+    public override void CSharpMarshalToNative(CSharpMarshalContext ctx)
+    {
+        ctx.Return.Write(ctx.Parameter.Name);
+    }
+
+    public override string CSharpConstruct() => "";
+}
